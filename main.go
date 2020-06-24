fe3cce8381a34e10157d11812183898ec5552a30
Sebastião Pamplona <sebastiao.pamplona@freiheit.com>
2020-06-24 17:34:47 +0100

    Created feat_1

---
+++ main.go
@@ -0,0 +1,19 @@
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("unimplemented")	
+}
+
+func pt1() {
+	fmt.Println("unimplemented")
+}
+
+func pt2() {
+  fmt.Println("unimplemented")
+}
+
+func pt3() {
+  fmt.Println("unimplemented")
+}
