785686edb72e4cda761db39e317f01e38011268b
Sebastião Pamplona <sebastiao.pamplona@freiheit.com>
2020-06-24 17:50:39 +0100

    Added feature 1, base

---
+++ main.go
@@ -0,0 +1,20 @@
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("unimplemented")	
+}
+
+func pt1() {
+	fmt.Println("unimplemented")
+}
+
+func pt2() {
+  fmt.Println("unimplemented")
+}
+
+func pt3() {
+  fmt.Println("unimplemented")
+}
+
