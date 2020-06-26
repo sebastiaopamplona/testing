61e7b49c7b4be2e6405dc23b826b006bd7683a54
Sebastião Pamplona <sebastiao.pamplona@freiheit.com>
2020-06-26 20:11:07 +0100

    Part 0

---
+++ client.go
@@ -0,0 +1,30 @@
+{
+	"clients": [
+		{
+			"name": "cli1",
+			"host": ":1"
+		},
+		{
+			"name": "cli2",
+			"host": ":2"
+		},
+		{
+			"name": "cli3",
+			"host": ":3"
+		},
+	],
+	"servers": [
+		{
+			"name": "srv1",
+			"host": "FIXME"
+		},
+		{
+			"name": "srv2",
+			"host": "FIXME"
+		},
+		{
+			"name": "srv3",
+			"host": "FIXME"
+		}
+	]
+}
